ba5d7bd59b76c682cfea06b46d70a2ff2c686ea7
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2017-12-05 11:39:48 -0500

    removed iief

--- 2017/03-spiral-memory/index.ts
+++ 2017/03-spiral-memory/index.ts
@@ -1,84 +1,83 @@
 /* logic:
- * unfold the square as a line and identify each value which is a corner.
- * identify the surrounding corners of your value
- * find the middle between the two corners
- * add to your distance the steps needed for the position of your value to reach the middle position
- * then add the value of the layer of the highest surrounding corner
- */
-(function () {
-  type CornerName = 'ur' | 'ul' | 'dl' | 'dr';
-  type Corner = {
-    corner: CornerName;
-    layer: number;
-    value: number;
-  };
-  const cornerNamess: CornerName[] = ['ur', 'ul', 'dl', 'dr'];
+* unfold the square as a line and identify each value which is a corner.
+* identify the surrounding corners of your value
+* find the middle between the two corners
+* add to your distance the steps needed for the position of your value to reach the middle position
+* then add the value of the layer of the highest surrounding corner
+*/
 
-  const corners = {
-    *[Symbol.iterator]() { // tslint:disable-line function-name
-      let i = 0;
-      let value = 1;
+type CornerName = 'ur' | 'ul' | 'dl' | 'dr';
+type Corner = {
+  corner: CornerName;
+  layer: number;
+  value: number;
+};
+const cornerNamess: CornerName[] = ['ur', 'ul', 'dl', 'dr'];
 
-      while (true) {
-        const layer = Math.floor(i / 4);
+const corners = {
+  *[Symbol.iterator]() { // tslint:disable-line function-name
+    let i = 0;
+    let value = 1;
 
-        yield {
-          layer,
-          corner: cornerNamess[i++ % 4],
-          value: value += 2 * layer,
-        } as Corner;
-      }
-    },
-  };
+    while (true) {
+      const layer = Math.floor(i / 4);
 
-  const getSurroundingCorners = (num: number): Corner[] => {
-    let precedingCorner: Corner = {
-      layer: 0,
-      corner: 'dr',
-      value: 1,
-    };
-
-    if (num <= 1) {
-      return [precedingCorner, undefined];
+      yield {
+        layer,
+        corner: cornerNamess[i++ % 4],
+        value: value += 2 * layer,
+      } as Corner;
     }
+  },
+};
 
-    for (const corner of corners) {
-      if (num <= corner.value) {
-        return [precedingCorner, corner];
-      }
-
-      precedingCorner = corner;
-    }
+const getSurroundingCorners = (num: number): Corner[] => {
+  let precedingCorner: Corner = {
+    layer: 0,
+    corner: 'dr',
+    value: 1,
   };
 
-  const getDistance = (num: number) => {
-    const [cornerBefore, cornerAfter] = getSurroundingCorners(num);
+  if (num <= 1) {
+    return [precedingCorner, undefined];
+  }
 
-    if (!cornerAfter) {
-      return 0;
+  for (const corner of corners) {
+    if (num <= corner.value) {
+      return [precedingCorner, corner];
     }
 
-    if (num === cornerBefore.value) {
-      return cornerBefore.layer * 2;
-    }
+    precedingCorner = corner;
+  }
+};
 
-    if (num === cornerAfter.value) {
-      return cornerAfter.layer * 2;
-    }
+const getDistance = (num: number) => {
+  const [cornerBefore, cornerAfter] = getSurroundingCorners(num);
 
-    const middleValue = cornerBefore.value + Math.floor((cornerAfter.value - cornerBefore.value) / 2);
+  if (!cornerAfter) {
+    return 0;
+  }
 
-    return cornerAfter.layer + Math.abs(num - middleValue);
-  };
+  if (num === cornerBefore.value) {
+    return cornerBefore.layer * 2;
+  }
 
-  console.log(`1: distance ${getDistance(1)}`);
-  console.log(`7: distance ${getDistance(7)}`);
-  console.log(`9: distance ${getDistance(9)}`);
-  console.log(`12: distance ${getDistance(12)}`);
-  console.log(`23: distance ${getDistance(23)}`);
-  console.log(`65: distance ${getDistance(65)}`);
-  console.log(`81: distance ${getDistance(81)}`);
-  console.log(`82: distance ${getDistance(82)}`);
-  console.log(`1024: distance ${getDistance(1024)}`);
-  console.log(`277678: distance ${getDistance(277678)}`);
-})();
+  if (num === cornerAfter.value) {
+    return cornerAfter.layer * 2;
+  }
+
+  const middleValue = cornerBefore.value + Math.floor((cornerAfter.value - cornerBefore.value) / 2);
+
+  return cornerAfter.layer + Math.abs(num - middleValue);
+};
+
+console.log(`1: distance ${getDistance(1)}`);
+console.log(`7: distance ${getDistance(7)}`);
+console.log(`9: distance ${getDistance(9)}`);
+console.log(`12: distance ${getDistance(12)}`);
+console.log(`23: distance ${getDistance(23)}`);
+console.log(`65: distance ${getDistance(65)}`);
+console.log(`81: distance ${getDistance(81)}`);
+console.log(`82: distance ${getDistance(82)}`);
+console.log(`1024: distance ${getDistance(1024)}`);
+console.log(`277678: distance ${getDistance(277678)}`);
--- 2017/03-spiral-memory/part-2.ts
+++ 2017/03-spiral-memory/part-2.ts
@@ -1,84 +1,82 @@
 /* logic:
- * generate an object where the keys are the cartesian coordinates which maps to the value at the coordinates
- * e.g. { x_y: value } ==> { '-1_-2': 22 }
- * for each layer, create an array of the coordinates e.g. [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1], [0,-1], [1,-1]]
- * for each coordinate, get the sum of their surrounding coordinates that have been created so far
- * add to the global object and check if value is greater than input, if not move to the next layer when layer is complete
- */
+* generate an object where the keys are the cartesian coordinates which maps to the value at the coordinates
+* e.g. { x_y: value } ==> { '-1_-2': 22 }
+* for each layer, create an array of the coordinates e.g. [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1], [0,-1], [1,-1]]
+* for each coordinate, get the sum of their surrounding coordinates that have been created so far
+* add to the global object and check if value is greater than input, if not move to the next layer when layer is complete
+*/
 
-(function () {
-  const values: {[xy: string]: number} = {
-    '0_0': 1,
-  };
+const values: {[xy: string]: number} = {
+  '0_0': 1,
+};
 
-  const getLayerCoordinates = (layer: number): [number, number][] => {
-    const positions = [];
-    let x = layer;
-    let y = 0 - (layer - 1);
+const getLayerCoordinates = (layer: number): [number, number][] => {
+  const positions = [];
+  let x = layer;
+  let y = 0 - (layer - 1);
 
-    positions.push([x, y]);
+  positions.push([x, y]);
 
-    while (y < layer) {
-      positions.push([x, ++y]);
+  while (y < layer) {
+    positions.push([x, ++y]);
+  }
+
+  while (x > (0 - layer)) {
+    positions.push([--x, y]);
+  }
+
+  while (y > (0 - layer)) {
+    positions.push([x, --y]);
+  }
+
+  while (x < layer) {
+    positions.push([++x, y]);
+  }
+
+  return positions;
+};
+
+const getSurroundingSum = (x: number, y: number): number => {
+  const surroundingKeys = [
+    [x - 1, y + 1],
+    [x - 1, y],
+    [x - 1, y - 1],
+    [x , y - 1],
+    [x + 1, y - 1],
+    [x + 1, y],
+    [x + 1, y + 1],
+    [x, y + 1],
+  ].map(pair => pair.join('_'));
+
+  return surroundingKeys.reduce((acc, key) => values[key] ? acc + values[key] : acc, 0);
+};
+
+const findFirstLargerValue = (puzzleInput: number, position = 2, layer = 1): number => {
+  if (position > puzzleInput) {
+    console.error('Could not find value larger than puzzle input.');
+    return;
+  }
+
+  const layerPositions = getLayerCoordinates(layer);
+
+  for (const [x, y] of layerPositions) {
+    const cur = getSurroundingSum(x, y);
+    values[`${x}_${y}`] = cur;
+
+    if (cur > puzzleInput) {
+      return cur;
     }
+  }
 
-    while (x > (0 - layer)) {
-      positions.push([--x, y]);
-    }
+  return findFirstLargerValue(puzzleInput, position + layerPositions.length, layer + 1);
+};
 
-    while (y > (0 - layer)) {
-      positions.push([x, --y]);
-    }
+const firstLargerValue = findFirstLargerValue(277678);
 
-    while (x < layer) {
-      positions.push([++x, y]);
-    }
-
-    return positions;
-  };
-
-  const getSurroundingSum = (x: number, y: number): number => {
-    const surroundingKeys = [
-      [x - 1, y + 1],
-      [x - 1, y],
-      [x - 1, y - 1],
-      [x , y - 1],
-      [x + 1, y - 1],
-      [x + 1, y],
-      [x + 1, y + 1],
-      [x, y + 1],
-    ].map(pair => pair.join('_'));
-
-    return surroundingKeys.reduce((acc, key) => values[key] ? acc + values[key] : acc, 0);
-  };
-
-  const findFirstLargerValue = (puzzleInput: number, position = 2, layer = 1): number => {
-    if (position > puzzleInput) {
-      console.error('Could not find value larger than puzzle input.');
-      return;
-    }
-
-    const layerPositions = getLayerCoordinates(layer);
-
-    for (const [x, y] of layerPositions) {
-      const cur = getSurroundingSum(x, y);
-      values[`${x}_${y}`] = cur;
-
-      if (cur > puzzleInput) {
-        return cur;
-      }
-    }
-
-    return findFirstLargerValue(puzzleInput, position + layerPositions.length, layer + 1);
-  };
-
-  const firstLargerValue = findFirstLargerValue(277678);
-
-  console.log(`1: ${values['1_0']}`);
-  console.log(`5: ${values['-1_1']}`);
-  console.log(`26: ${values['2_-1']}`);
-  console.log(`122: ${values['1_2']}`);
-  console.log(`362: ${values['-2_-2']}`);
-  console.log(`806: ${values['0_-2']}`);
-  console.log(`first larger value: ${firstLargerValue}`);
-})();
+console.log(`1: ${values['1_0']}`);
+console.log(`5: ${values['-1_1']}`);
+console.log(`26: ${values['2_-1']}`);
+console.log(`122: ${values['1_2']}`);
+console.log(`362: ${values['-2_-2']}`);
+console.log(`806: ${values['0_-2']}`);
+console.log(`first larger value: ${firstLargerValue}`);
