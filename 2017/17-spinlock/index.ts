65d29b74a9040a84b649a183448171ff7612aad1
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2017-12-18 14:06:51 -0500

    part two

--- 2017/17-spinlock/index.ts
+++ 2017/17-spinlock/index.ts
@@ -1,10 +1,10 @@
-const getShortCircuit = (steps: number, size: number): number => {
+const getShortCircuitPartOne = (steps: number): number => {
   const buffer = [0];
   let position = 0;
   let zero = 0;
   let afterZero;
 
-  for (let i = 1; i <= size; ++i) {
+  for (let i = 1; i <= 2017; ++i) {
     position = (position + steps + 1) % buffer.length;
     buffer.splice(position, 0, i);
 
@@ -18,6 +18,24 @@ const getShortCircuit = (steps: number, size: number): number => {
   return buffer[(position + 1) % buffer.length];
 };
 
-console.log(`Test should be 638: ${getShortCircuit(3, 2017)}`);
-console.log(`Result ${getShortCircuit(376, 2017)}`);
-console.log(`Result ${getShortCircuit(376, 50000000)}`);
+const getShortCircuitPartTwo = (steps: number, size: number): number => {
+  let val = 0;
+  let pos = 0;
+
+  for (let i = 1; i < size; ++i) {
+    pos = (pos + steps) % (i);
+
+    if (pos === 0) {
+      val = i;
+    }
+
+    ++pos;
+  }
+
+  return val;
+};
+
+console.log(`Test should be 638: ${getShortCircuitPartOne(3)}`);
+console.log(`Result ${getShortCircuitPartOne(376)}`);
+console.log(`Test should be 16: ${getShortCircuitPartTwo(3, 70)}`);
+console.log(`Result ${getShortCircuitPartTwo(376, 50000000)}`);
