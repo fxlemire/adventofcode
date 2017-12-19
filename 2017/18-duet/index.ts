94910b1267b1bfa8c86fd54ad7ccbeed48987681
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2017-12-18 19:30:12 -0500

    part 2 ongoing

--- 2017/18-duet/index.ts
+++ 2017/18-duet/index.ts
@@ -38,6 +38,8 @@ const getInstructions = (file: string): Promise<{registers: {[key: string]: numb
   rl.on('close', () => res({ registers, instructions }));
 });
 
+/* Part One */
+
 const getFirstRecoveredFrequency = async (file: string): Promise<number> => {
   const { registers, instructions } = await getInstructions(file);
   const getValue = (a: string|number): number => (Number.isInteger(a as number) ? a : registers[a]) as number;
@@ -92,7 +94,109 @@ const getFirstRecoveredFrequency = async (file: string): Promise<number> => {
   return lastFrequencyPlayed;
 };
 
+/* Part Two */
+
+class Program {
+  private id: number; // tslint:disable-line no-unused-variable
+  private instructions: Instruction[];
+  private lastInstructionExecuted: number = -1;
+  private messageQueue: number[] = [];
+  private registers: {[key: string]: number};
+  private transmissions: number = 0;
+
+  constructor(id: number, registers: {[key: string]: number}, instructions: Instruction[]) {
+    this.id = id;
+    this.registers = { ...registers };
+    this.registers['p'] = id;
+    this.instructions = [...instructions];
+  }
+
+  private getValue(a: string|number): number {
+    return (Number.isInteger(a as number) ? a : this.registers[a]) as number;
+  }
+
+  addMessage(n: number) {
+    this.messageQueue.push(n);
+  }
+
+  execute(pairedProgram: Program): boolean {
+    if (this.lastInstructionExecuted < -1 || this.lastInstructionExecuted >= this.instructions.length - 1) {
+      return false;
+    }
+
+    const instruction = this.instructions[++this.lastInstructionExecuted];
+
+    switch (instruction.action) {
+      case 'add': {
+        this.registers[instruction.args[0]] += this.getValue(instruction.args[1]);
+        break;
+      }
+      case 'jgz': {
+        const regValue = this.getValue(instruction.args[0]);
+
+        if (regValue > 0) {
+          this.lastInstructionExecuted += (this.getValue(instruction.args[1]) - 1);
+        }
+
+        break;
+      }
+      case 'mod': {
+        this.registers[instruction.args[0]] %= this.getValue(instruction.args[1]);
+        break;
+      }
+      case 'mul': {
+        this.registers[instruction.args[0]] *= this.getValue(instruction.args[1]);
+        break;
+      }
+      case 'rcv': {
+        if (this.messageQueue.length <= 0) {
+          --this.lastInstructionExecuted;
+          return false;
+        }
+
+        this.registers[instruction.args[0]] = this.messageQueue.shift();
+      }
+      case 'set': {
+        this.registers[instruction.args[0]] = this.getValue(instruction.args[1]);
+        break;
+      }
+      case 'snd': {
+        pairedProgram.addMessage(this.getValue(instruction.args[0]));
+        ++this.transmissions;
+        break;
+      }
+      default:
+        throw new Error('Invalid instruction');
+    }
+
+    return true;
+  }
+
+  getTransmissions() {
+    return this.transmissions;
+  }
+}
+
+const getProgram1TotalTransmissions = async (file: string): Promise<number> => {
+  const { registers, instructions } = await getInstructions(file);
+  const p0 = new Program(0, registers, instructions);
+  const p1 = new Program(1, registers, instructions);
+
+  let isDeadLocked = false;
+
+  while (!isDeadLocked) {
+    while (p0.execute(p1));
+    while (p1.execute(p0));
+
+    isDeadLocked = !p0.execute(p1) && !p1.execute(p0);
+  }
+
+  return p1.getTransmissions();
+};
+
 (async function () {
   console.log(`Test should be 4: ${await getFirstRecoveredFrequency('test')}`);
   console.log(`Result: ${await getFirstRecoveredFrequency('duet')}`);
+  console.log(`Test should be 3: ${await getProgram1TotalTransmissions('test2')}`);
+  console.log(`Result: ${await getProgram1TotalTransmissions('duet')}`);
 })();
